004073c26989fcde70148ccb0d8a5ea00b801f25
Karath <vorkras@gmail.com>
2016-03-02 12:38:29 +0100

    small fix in crawler

--- SearchEngine/BLL/FileCrawler.cs
+++ SearchEngine/BLL/FileCrawler.cs
@@ -13,92 +13,52 @@ namespace BLL
 {
     class FileCrawler
     {
-<<<<<<< HEAD
         private IDocumentRepository docRepo = new Facade().GetDocumentRepository();
 
         private String URL;
         private DateTime time;
-         public void Filereading(DocumentDTO documentdto)
+        public void Filereading(DocumentDTO documentdto)
         {
             string text = documentdto.URL;
             crawl(text);
         }
-    
-    
-    //static int count = 0;
-    //private static void crawl(string path)
-    //{
-    //    var start = DateTime.UtcNow.Millisecond;
 
-    //    crawl(path, "");
-    //    var end = DateTime.UtcNow.Millisecond;
 
-    //    long timeSpan = (end - start) / 1000;
+        //static int count = 0;
+        //private static void crawl(string path)
+        //{
+        //    var start = DateTime.UtcNow.Millisecond;
 
-    //}
+        //    crawl(path, "");
+        //    var end = DateTime.UtcNow.Millisecond;
 
-    public void crawl(string path)
-    {
-        if (Directory.Exists(path))
+        //    long timeSpan = (end - start) / 1000;
+
+        //}
+
+        public void crawl(string path)
         {
-            string[] subFiles = Directory.GetFiles(path);
-            
-            for (int i = 0; i < subFiles.Length; i++)
+            if (Directory.Exists(path))
             {
-                crawl(subFiles[i]);
-                
-            }
-            foreach (string s in subFiles)
-            {
-                URL = s;
-                time = File.GetCreationTimeUtc(s);
-                Document doc = docRepo.CreateDocument(new Document
-                 {
-                     URL = URL,
-                     IndexTime = time
-                 });
-              
+                string[] subFiles = Directory.GetFiles(path);
+
+                for (int i = 0; i < subFiles.Length; i++)
+                {
+                    crawl(subFiles[i]);
+
+                }
+                foreach (string s in subFiles)
+                {
+                    URL = s;
+                    time = File.GetCreationTimeUtc(s);
+                    Document doc = docRepo.CreateDocument(new Document
+                     {
+                         URL = URL,
+                         IndexTime = time
+                     });
+
+                }
             }
         }
-    }
-=======
-    //     public void Filereading()
-    //    {
-    //        string text = "Documents";
-    //        crawl(f);
-    //    }
-    
-    
-    //static int count = 0;
-    //public static void crawl(File f)
-    //{
-    //    DateTime start = new DateTime();
-        
-    //	crawl(f, "");
-    //   DateTime end = new DateTime();
-    //    long timeSpan = (end.Date - start.Date)/1000;
-    //    System.out.println("Renaming finished in " + timeSpan + "s. " + count + " docs renamed.");
-    //}
-    
-    //private static void crawl(File f, String indent)
-    //{
-    //	//System.out.println(indent + f.getName());
-    //	if (f.isDirectory()) {
-    //    	File[] subFiles = f.listFiles();
-    //		indent += "    ";
-    //    	for (int i = 0; i < subFiles.length; i++) {
-    //          	crawl(subFiles[i], indent);
-    //    	}
-    //    y
-    //    }
-    //    else 
-    //        if (f.isFile() && !f.isHidden())
-    //        {
-    //            f.renameTo(new File(f.getAbsolutePath()+"txt"));
-    //            count++;
-    //        }
-        
-    //}
->>>>>>> 849266fbeed5edf1e85dcc35722236e553f48cfe
     }
 }
